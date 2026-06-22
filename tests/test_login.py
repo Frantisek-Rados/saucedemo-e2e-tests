a1687a6616554800c3160e74505298694e316ac0
František Radoš <frantisek.rados2@gmail.com>
2026-06-22 11:30:27 +0200

    Pridany LoginPage class a test_login

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,10 +3,10 @@ from pages.login_page import LoginPage
 
 def test_login():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new.page()
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
         login_page = LoginPage(page)
         login_page.go_to()
-        login_page.login("standart_user", "secret_sauce")
-        assert page.url == "https://www.saucedemo.com/invertory.html"
+        login_page.login("standard_user", "secret_sauce")
+        assert page.url == "https://www.saucedemo.com/inventory.html"
         browser.close()
