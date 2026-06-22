19805c57fa6103c784bd9a07c9a69836b0ea7a28
František Radoš <frantisek.rados2@gmail.com>
2026-06-22 11:08:06 +0200

    Pridana zakladna struktura projektu

---
+++ tests/test_login.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+from pages.login_page import LoginPage
+
+def test_login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new.page()
+        login_page = LoginPage(page)
+        login_page.go_to()
+        login_page.login("standart_user", "secret_sauce")
+        assert page.url == "https://www.saucedemo.com/invertory.html"
+        browser.close()
