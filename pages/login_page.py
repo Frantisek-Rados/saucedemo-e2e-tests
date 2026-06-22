b3936c476c9f4f6298e58bf0081872b11ee2a560
František Radoš <frantisek.rados2@gmail.com>
2026-06-22 11:40:59 +0200

    Opraveny login_page a test_login - odstranene konfliktne riadky

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from playwright.sync_api import Page
 
 class LoginPage:
@@ -15,21 +14,3 @@ class LoginPage:
         self.page.fill(self.username_input, username)
         self.page.fill(self.password_input, password)
         self.page.click(self.login_button)
-=======
-from playwright.sync_api import Page
-
-class LoginPage:
-    def __init__(self, page: Page):
-        self.page = page
-        self.username_input = "#user-name"
-        self.password_input = "#password"
-        self.login_button = "#login-button"
-
-    def go_to(self):
-        self.page.goto("https://www.saucedemo.com/")
-
-    def login(self, username: str, password: str):
-        self.page.fill(self.username_input, username)
-        self.page.fill(self.password_input, password)
-        self.page.click(self.login_button)
->>>>>>> 0c2f889524e78c50a143bee37a9593a0d1e6485e
