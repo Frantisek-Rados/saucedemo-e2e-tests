0c2f889524e78c50a143bee37a9593a0d1e6485e
František Radoš <frantisek.rados1@gmail.com>
2026-06-22 11:23:18 +0200

    Fix instantiation of LoginPage in test_login

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,7 +5,7 @@ def test_login():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new.page()
-        login_page = LoginPage(page)
+        login_page = login_page(page)
         login_page.go_to()
         login_page.login("standart_user", "secret_sauce")
         assert page.url == "https://www.saucedemo.com/invertory.html"
